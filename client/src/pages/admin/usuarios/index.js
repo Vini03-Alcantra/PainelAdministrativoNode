932b4a1db5b8cffa6cbe22d566c1f9d53fb064a9
Vinicius de Alcantra Barros <viniciusalcantarabarros@gmail.com>
2021-08-12 12:57:29 -0300

    Configurando navegaçoes

--- client/src/pages/admin/usuarios/index.js
+++ client/src/pages/admin/usuarios/index.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function UsuariosListagem(){
     return (  
-        <div><h1>Listagem de Produtos</h1></div>
+        <div><h1>Listagem de usuarios</h1></div>
     )
 }
